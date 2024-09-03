ab855761eaa566108e0655e5bcf086bab794cf12
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-09-03 14:35:45 -0500

    aside tables y computadores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'azul-claro':'rgb(14 165 233)',
+        'azul-hover':'rgb(25 106 221 / 55%)'
+       
+      },
+    },
   },
   plugins: [],
 }
