dd2bd192a7594270eeb00f889f5ac65ccb7c2fba
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-09-05 20:19:07 -0500

    Creé el main y cambié unos estilos

--- src/js/script.js
+++ src/js/script.js
@@ -5,19 +5,51 @@ const btnBars = document.getElementById('btnBars');
 const aside = document.getElementById('aside');
 const btnCloseAside = document.getElementById('btnCloseAside');
 
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+const bodyElement = document.documentElement;
+const sunIcon = document.getElementById('sun-icon');
+//const moonIcon = document.getElementById('moon-icon');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+  bodyElement.classList.add('dark');
+  sunIcon.classList.remove('text-yellow-500');
+} else {
+  bodyElement.classList.remove('dark');
+  sunIcon.classList.add('text-yellow-500');
+}
+
+toggleButton.addEventListener('click', () => {
+  const isDarkMode = bodyElement.classList.toggle('dark');
+  if (isDarkMode) {
+    sunIcon.classList.remove('text-yellow-500');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    sunIcon.classList.add('text-yellow-500');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+//Click en el perfil para mostrar o cerrar  dropdown dle menu
 dropdownButton.addEventListener('click', function() {
     dropdownMenu.classList.toggle('hidden');
 });
 
+//Click fuera del menu dropdown para agregar hidden y cerrar menu
 document.addEventListener('click', function(event) {
     if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
         dropdownMenu.classList.add('hidden');
     }
 });
 
+//Cierra menu responsivo
 btnBars.addEventListener('click', () => {
     aside.classList.remove('hidden')
 })
+//muestra menu o sidebar responsivo
 btnCloseAside.addEventListener('click', () =>{
     aside.classList.add('hidden')
 })
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        'azul-claro':'rgb(14 165 233)',
-        'azul-hover':'rgb(25 106 221 / 55%)'
-       
+      colors: {
+        fondo: "#0C151D",
+        negro: "#171F26",
+        blanco:"#FFFFFF",
+        azul:"#14A1F0",
+        morado:{
+          100:"rgb(243 232 255)",
+        }
       },
+      spacing:{
+        '100':'600px'
+      }
     },
   },
   plugins: [],
